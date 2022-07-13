e109531bc7c96b330d92798f5b802fc297d02342
Jarrett Gray <jarrett.gray@uconn.edu>
2022-07-12 18:19:38 -0600

    restructured getUserRepos to have function call a user and run fetch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,13 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+var getUserRepos = function(user) {
+    // format the github api url 
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+
+    //make request to the URL
+    fetch(apiURL).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
     });
-
-    console.log("outside");
 }
 
-getUserRepos();
+getUserRepos("jhg13010");
