c250689e57d62db9cc5220dcbbcf4559aee65948
Jarrett Gray <jarrett.gray@uconn.edu>
2022-07-14 18:28:26 -0600

    created formSubmitHandler code

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username")
+
 var getUserRepos = function(user) {
     // format the github api url 
     var apiURL = "https://api.github.com/users/" + user + "/repos";
@@ -10,4 +13,21 @@ var getUserRepos = function(user) {
     });
 }
 
-getUserRepos("jhg13010");
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    //get name and call getUserRepos
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        //clear form
+        nameInputEl.value="";
+    //alert if empty or not a github user
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
